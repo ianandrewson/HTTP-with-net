d19747cd58a67cdc3bab1c9cb5c4d22710d8d953
Ian Andrewson <ianandrewson@gmail.com>
2019-12-09 18:06:13 -0800

    routes written for /, /green, /red, /blue

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,14 @@ const app = require('../lib/app');
 
 describe('createResponse', () => {
   it('should properly respond to GET requests to /', () => {
-    request(app)
+    return request(app)
       .get('/')
       .then(res => {
         expect(res.text).toEqual('hi');
       });
   });
   it('should properly respond to POST requests to /echo', () => {
-    request(app)
+    return request(app)
       .post('/echo')
       .then((req, res) => {
         expect(res.status.text).toEqual('status code 200');
@@ -18,24 +18,24 @@ describe('createResponse', () => {
       });
   });
   it('should properly respond to GET requests to /red', () => {
-    request(app)
+    return request(app)
       .get('/red')
       .then(res => {
-        expect(res.text).toEqual('<h1 style = color: red>red</h1>');
+        expect(res.text).toEqual('<h1 style="color:red">red</h1>');
       });
   });
   it('should properly respong to GET requests to /green', () => {
-    request(app)
+    return request(app)
       .get('/green')
       .then(res => {
-        expect(res.text).toEqual('<h1 style = color: green>green</h1>');
+        expect(res.text).toEqual('<h1 style="color:green">green</h1>');
       });
   });
   it('should properly respond to GET requests to /blue', () => {
-    request(app)
+    return request(app)
       .get('/blue')
       .then(res => {
-        expect(res.text).toEqual('<h1 style = color: blue>blue</h1>');
+        expect(res.text).toEqual('<h1 style="color:blue">blue</h1>');
       });
   });
 });
